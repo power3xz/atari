bc8c42ba7832fdd37c46ea013d05d66364c37d93
devj <power3xz@gmail.com>
2023-02-26 13:58:32 +0900

    ex4 추가

---
+++ ex/ex4.asm
@@ -0,0 +1,14 @@
+  processor 6502
+  seg Code
+  org $F000
+Start:
+  ; TODO:
+  ; Load the A register with the literal decimal value 100
+
+  ; Add the decimal value 5 to accumalator
+  ; Subtract the decimal value 10 from the accumulator
+  ; Register A should now contain the decimal 95 (or $5f in hexadecimal)
+
+  org $FFFC
+  .word Start
+  .word Start
