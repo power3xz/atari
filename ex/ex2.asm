8e65a01b00332698b2bcb72335e5e26af880ea42
devj <power3xz@gmail.com>
2023-02-26 13:23:12 +0900

    ex2 추가

---
+++ ex/ex2.asm
@@ -0,0 +1,12 @@
+  processor 6502
+  seg Code
+  org $F000
+Start:
+  ; TODO:
+  ; Load the A register with the hexadecimal value $A
+  ; Load the X register with the binary value %11111111
+  ; Store the value in the A register into memory address $80
+  ; Store the value in the X register into memory address $81
+  org $FFFC ; End the ROM by adding requied values to memory position $FFFC
+  .word Start ; Put 2 bytes with the reset address at memory position $FFFC
+  .word Start ; Put 2 bytes with the break address at memory position $FFFE
